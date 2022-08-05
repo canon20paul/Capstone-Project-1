1e2d008b526d44555adecf4c9857226be113b8ef
canon20paul <canon20paul@gmail.com>
2022-08-05 22:22:32 +0300

    js formT

--- speakers.js
+++ speakers.js
@@ -135,4 +135,4 @@ const appendWorksR = (speekerz, startt) => {
 workCard1 += ' <button type="button" data-work="${i}" class="btn buttonM" onclick=" appendworksR(speekerz,${[i]});"> <p>Less &nbsp;<span class="downA">V</span></p> </button> ';
 };
 // appendWorksR(speekerz,5);
-  projectsC.innerHTML += workCard1;
+projectsC.innerHTML += workCard1;
